df85111009da1b5a7fdd8af3e39274f3d23e7d18
TomRobert <benjamin.scheiwiller@hotmail.com>
2020-09-21 19:21:54 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -3,6 +3,8 @@ sets:[
 'set_free_to_use_dummy',
 'set_xmas2015_clothing',
 '2018_achievement_set',
+],max: 387,
+unlock: [
 'oktoberfest_set_2020_1',
 'oktoberfest_set_2020_2',
 'oktoberfest_set_2020_3',
@@ -14,16 +16,11 @@ sets:[
 'oktoberfest_set_2020_9',
 'oktoberfest_set_2020_10',
 'oktoberfest_set_2020_11',
-],max: 376,
-unlock: [
-'harvester_set_clothing',
-'harvester_set_animal',
-'harvester_set_weapon',
 ],IDs:[
 '1337',
 '41999',
 '50106',
-],maxID:52149,
-unlockID:52118,
-date:new Date('2020-08-17'),
+],maxID:52218,
+unlockID:52149,
+date:new Date('2020-09-17'),
 };if(['Tom Robert','Fanta'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_free_to_use_dummy','set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
