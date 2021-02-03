ad797f00496e68541f9ffcaf626da672b5b811ba
TomRobert <benjamin.scheiwiller@hotmail.com>
2021-02-03 19:53:07 +0100

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -2,17 +2,15 @@ forbid={
 sets:[
 'set_free_to_use_dummy',
 'set_xmas2015_clothing',
-],max: 402,
+],max: 404,
 unlock: [
-'fan_collector_set',
-'christmas_set_1_2020',
-'christmas_set_2_2020',
-'christmas_set_3_2020',
+'curling_set_1',
+'curling_set_2',
 ],IDs:[
 '1337',
 '41999',
 '50106',
-],maxID:52313,
-unlockID:52266,
-date:new Date('2020-12-16'),
+],maxID:52336,
+unlockID:52313,
+date:new Date('2021-02-02'),
 };if(['Tom Robert','Fanta'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_free_to_use_dummy','set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
