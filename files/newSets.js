925bcdacc69d20ff4f171a0b324b72898ee5cf3f
TomRobert <benjamin.scheiwiller@hotmail.com>
2021-04-03 21:52:56 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -2,6 +2,8 @@ forbid={
 sets:[
 'set_free_to_use_dummy',
 'set_xmas2015_clothing',
+],max: 431,
+unlock: [
 'easter_chick_set_1',
 'easter_chick_set_2',
 'easter_sheep_set_1',
@@ -18,20 +20,11 @@ sets:[
 'easter_2021_set_8',
 'easter_2021_set_9',
 'easter_2021_set_10',
-],max: 415,
-unlock: [
-'west_fun_animal_set',
-'curling_set_2',
-'legendary_set_1',
-'legendary_set_2',
-'eire_set_1',
-'eire_set_2',
-'eire_set_3',
 ],IDs:[
 '1337',
 '41999',
 '50106',
-],maxID:52409,
+],maxID:52491,
 unlockID:52379,
 date:new Date('2021-03-16'),
-};if(['Tom Robert','Fanta'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_free_to_use_dummy','set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
+};if(['Tom Robert','Fanta'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
