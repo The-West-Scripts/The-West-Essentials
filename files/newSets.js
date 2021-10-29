679caf8021dfa72068d0aeaf20709c9aee4bb95c
TomRobert <benjamin.scheiwiller@hotmail.com>
2021-10-29 20:03:06 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -2,6 +2,8 @@ forbid={
 sets:[
 'set_free_to_use_dummy',
 'set_xmas2015_clothing',
+],max: 469,
+unlock: [
 'dod_2021_set_1',
 'dod_2021_set_2',
 'dod_2021_set_3',
@@ -13,23 +15,11 @@ sets:[
 'dod_2021_set_9',
 'fancy_set_2021',
 'green_set_2021',
-],max: 458,
-unlock: [
-'octoberfest_2021_set_1',
-'octoberfest_2021_set_2',
-'octoberfest_2021_set_3',
-'octoberfest_2021_set_4',
-'octoberfest_2021_set_5',
-'octoberfest_2021_set_6',
-'octoberfest_2021_set_7',
-'octoberfest_2021_set_8',
-'octoberfest_2021_set_9',
-'octoberfest_2021_set_10',
 ],IDs:[
 '1337',
 '41999',
 '50106',
-],maxID:52651,
-unlockID:52600,
-date:new Date('2021-09-16'),
+],maxID:52723,
+unlockID:52675,
+date:new Date('2021-10-28'),
 };if(['Tom Robert','Fanta'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
