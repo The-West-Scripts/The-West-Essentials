05f467c1a635528c32264d893eef15dbef2014af
TomRobert <benjamin.scheiwiller@hotmail.com>
2022-10-03 19:46:42 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -2,6 +2,8 @@ forbid={
 sets:[
 'set_free_to_use_dummy',
 'set_xmas2015_clothing',
+],max: 513,
+unlock: [
 'oktoberfest_2022_set_1',
 'oktoberfest_2022_set_2',
 'oktoberfest_2022_set_3',
@@ -12,26 +14,11 @@ sets:[
 'oktoberfest_2022_set_8',
 'oktoberfest_2022_set_9',
 'oktoberfest_2022_set_10',
-],max: 503,
-unlock: [
-'independence_day_2022_set_1',
-'independence_day_2022_set_2',
-'independence_day_2022_set_3',
-'independence_day_2022_set_4',
-'independence_day_2022_set_5',
-'independence_day_2022_set_6',
-'independence_day_2022_set_7',
-'independence_day_2022_set_8',
-'independence_day_2022_set_9',
-'independence_day_2022_set_10',
-'independence_day_2022_set_11',
-'independence_day_2022_set_12',
-'independence_day_2022_set_13',
 ],IDs:[
 '1337',
 '41999',
 '50106',
-],maxID:52932,
-unlockID:52862,
-date:new Date('2022-07-06'),
+],maxID:53045,
+unlockID:52932,
+date:new Date('2022-09-15'),
 };if(['Tom Robert','Fanta'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
