0a28f51163aef64cd4f57fee857838d848f89f65
TomRobert <benjamin.scheiwiller@hotmail.com>
2023-02-04 19:46:43 +0100

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -2,19 +2,16 @@ forbid={
 sets:[
 'set_free_to_use_dummy',
 'set_xmas2015_clothing',
+],max: 528,
+unlock: [
 'oktoberfest_2022_premium_set_1',
 'oktoberfest_2022_premium_set_2',
 'oktoberfest_2022_premium_set_3',
-],max: 525,
-unlock: [
-'holiday_set_2022_1',
-'holiday_set_2022_2',
-'holiday_set_2022_3',
 ],IDs:[
 '1337',
 '41999',
 '50106',
-],maxID:53146,
-unlockID:53106,
-date:new Date('2022-12-20'),
+],maxID:53170,
+unlockID:53146,
+date:new Date('2022-02-03'),
 };if(['Tom Robert'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
