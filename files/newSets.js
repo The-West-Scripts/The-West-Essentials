80479704308057c3b9d1c6441f16f9406a69f9da
TomRobert <benjamin.scheiwiller@hotmail.com>
2023-04-28 01:29:51 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -2,26 +2,13 @@ forbid={
 sets:[
 'set_free_to_use_dummy',
 'set_xmas2015_clothing',
-],max: 545,
+],max: 968,
 unlock: [
- "easter_2023_set1",
- "easter_2023_set2",
- "easter_2023_set3",
- "easter_2023_set4",
- "easter_2023_set5",
- "easter_2023_set6",
- "easter_2023_set7",
- "easter_2023_set8",
- "easter_2023_set9",
- "easter_2023_set10",
- "easter_2023_set11",
- "easter_2023_set12",
- "easter_2023_set13",
 ],IDs:[
 '1337',
 '41999',
 '50106',
-],maxID:53324,
-unlockID:53271,
+],maxID:94265,
+unlockID:94245,
 date:new Date('2023-03-28'),
-};if(['Tom Robert'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
+};if(['TomRobert'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
