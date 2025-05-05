efdef6bc359a23e4296bfc77ba432171e101e223
TomRobert <benjamin.scheiwiller@hotmail.com>
2025-05-05 23:45:16 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -1,14 +1,9 @@
-forbid={
-sets:[
-'set_free_to_use_dummy',
-'set_xmas2015_clothing',
-],max: 968,
-unlock: [
-],IDs:[
-'1337',
-'41999',
-'50106',
-],maxID:94265,
-unlockID:94245,
-date:new Date('2023-03-28'),
-};if(['TomRobert'].includes(Character.name)){forbid.unlock=forbid.unlock.concat(forbid.sets);forbid.sets=['set_xmas2015_clothing'];forbid.maxID=253801;forbid.max=999;}
+TWX.forbid={
+sets:['set_xmas2015_clothing', 'Rowtag_set'],
+max: 968,
+//unlock: [],
+IDs:[138', '860', '1337', '41999', '50106', '50260'],
+maxID:94265,
+//unlockID:94245,
+//date:new Date('2023-03-28'),
+};if(Game.environment=='alpha'){forbid.sets=[];forbid.IDs=['50106'];}
