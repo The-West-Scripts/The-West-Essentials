e62a600b80225a08f860fcefca8650bbbf78c2fc
TomRobert <benjamin.scheiwiller@hotmail.com>
2025-05-06 00:00:18 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -1,4 +1,4 @@
-TWX.forbid={
+forbid={
 sets:['set_xmas2015_clothing', 'Rowtag_set'],
 max: 968,
 //unlock: [],
@@ -6,4 +6,4 @@ IDs:[138, 860, 1337, 41999, 50106, 50260],
 maxID:94265,
 //unlockID:94245,
 //date:new Date('2023-03-28'),
-};if(Game.environment=='alpha'){TWX.forbid.sets=[];TWX.forbid.IDs=[50106];}
+};if(Game.environment=='alpha'){forbid.sets=[];forbid.IDs=[50106];}
