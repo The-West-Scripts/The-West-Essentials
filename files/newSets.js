45dfbaf7d6f19a53d76c76638951d1b467b42da1
TomRobert <benjamin.scheiwiller@hotmail.com>
2025-05-05 23:49:47 +0200

    Update newSets.js

--- files/newSets.js
+++ files/newSets.js
@@ -2,7 +2,7 @@ TWX.forbid={
 sets:['set_xmas2015_clothing', 'Rowtag_set'],
 max: 968,
 //unlock: [],
-IDs:[138', '860', '1337', '41999', '50106', '50260'],
+IDs:[138, 860, 1337, 41999, 50106, 50260],
 maxID:94265,
 //unlockID:94245,
 //date:new Date('2023-03-28'),
